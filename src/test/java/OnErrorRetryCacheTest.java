405bdf880f8f1190d93355d651f655d6544409ba
Plato Balomenakis <plato.blm@gmail.com>
2015-11-15 17:58:12 +0000

    improve test

--- src/test/java/OnErrorRetryCacheTest.java
+++ src/test/java/OnErrorRetryCacheTest.java
@@ -2,15 +2,15 @@ import org.junit.Before;
 import org.junit.Test;
 import rx.Observable;
 import rx.Subscriber;
+import rx.schedulers.Schedulers;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static rx.schedulers.Schedulers.computation;
 
 public class OnErrorRetryCacheTest {
 
-    private static final int TASK_DURATION = 500;
+    private static final int TASK_DURATION = 200;
 
     AtomicInteger attempts = new AtomicInteger();
 
@@ -20,76 +20,57 @@ public class OnErrorRetryCacheTest {
  
     Observable<Integer> longTask;
     
-    @Before
-    public void setup(){
+    @Before public void setup(){
         longTask = Observable.just(0)
                 .map(x -> {
                     attempts.incrementAndGet();
                     sleep(TASK_DURATION);
                     return 0;
                 })
-                .subscribeOn(computation());
+                .subscribeOn(Schedulers.newThread());
     }
     
-    @Test
-    public void shouldExecuteOnceAndCacheResultIfFirstCompletes() {
+    @Test public void shouldExecuteOnceAndCacheResultIfFirstCompletes() 
+    {
+        int expectedAttempts = 1; // 1st succeeds and is cached
         Observable<Integer> observable = OnErrorRetryCache.from(longTask);
-        int expectedAttempts = 1;// just the first that completed and was cached
 
         subscribeAllAndWaitUntilDone(observable);
 
         assertThat(attempts.get()).isEqualTo(expectedAttempts);
-        assertThat(subscriber1.next).isTrue();
-        assertThat(subscriber2.next).isTrue();
-        assertThat(subscriber3.next).isTrue();
+        assertThat(subscriber1.nextCalled).isTrue();
+        assertThat(subscriber2.nextCalled).isTrue();
+        assertThat(subscriber3.nextCalled).isTrue();
     }
 
-    @Test
-    public void shouldRetryAndThenCacheResultIfFirstFails() {
-        Observable<Integer> observable = longTask
-                .map(x -> {
-                    failOnFirstAttempt();
-                    return 0;
-                });
-        observable = OnErrorRetryCache.from(observable);
-        int expectedAttempts = 2;// the first that failed and the second that completed and was cached
+    @Test public void shouldRetryAndThenCacheResultIfFirstFails() 
+    {
+        int expectedAttempts = 2; // 1st fails, 2nd succeeds and is cached
+        Observable<Integer> observable = OnErrorRetryCache.from(
+                taskThatFailsOnFirstAttempt()
+        );
 
         subscribeAllAndWaitUntilDone(observable);
 
         assertThat(attempts.get()).isEqualTo(expectedAttempts);
-        assertThat(subscriber1.error).isTrue();
-        assertThat(subscriber2.next).isTrue();
-        assertThat(subscriber3.next).isTrue();
+        assertThat(subscriber1.errorCalled).isTrue();
+        assertThat(subscriber2.nextCalled).isTrue();
+        assertThat(subscriber3.nextCalled).isTrue();
     }
 
-    @Test
-    public void shouldRetryIfFirstTwoFail() {
-        Observable<Integer> observable = longTask
-                .map(x -> {
-                    failOnFirstTwoAttempts();
-                    return 0;
-                });
-        observable = OnErrorRetryCache.from(observable);
-        int expectedAttempts = 3; // the first two that failed and the third that completed
+    @Test public void shouldRetryIfFirstTwoFail() 
+    {
+        int expectedAttempts = 3; // 1st and 2nd fail, 3rd succeeds
+        Observable<Integer> observable = OnErrorRetryCache.from(
+                taskThatFailsOnFirstTwoAttempts()
+        );
 
         subscribeAllAndWaitUntilDone(observable);
 
         assertThat(attempts.get()).isEqualTo(expectedAttempts);
-        assertThat(subscriber1.error).isTrue();
-        assertThat(subscriber2.error).isTrue();
-        assertThat(subscriber3.next).isTrue();
-    }
-
-    private void failOnFirstAttempt() {
-        if (attempts.get() == 1) {
-            throw new RuntimeException();
-        }
-    }
-
-    private void failOnFirstTwoAttempts() {
-        if (attempts.get() <= 2) {
-            throw new RuntimeException();
-        }
+        assertThat(subscriber1.errorCalled).isTrue();
+        assertThat(subscriber2.errorCalled).isTrue();
+        assertThat(subscriber3.nextCalled).isTrue();
     }
 
     private void subscribeAllAndWaitUntilDone(Observable<Integer> observable) {
@@ -100,33 +81,36 @@ public class OnErrorRetryCacheTest {
     }
     
     private static class TestSubscriber extends Subscriber {
-        boolean completed;
-        boolean error;
-        boolean next;
+        boolean completedCalled;
+        boolean errorCalled;
+        boolean nextCalled;
 
         @Override
         public void onCompleted() {
-            completed = true;
+            completedCalled = true;
         }
 
         @Override
         public void onError(Throwable e) {
-            error = true;
+            errorCalled = true;
         }
 
         @Override
         public void onNext(Object o) {
-            next = true;
+            nextCalled = true;
         }
 
         boolean done() {
-            return completed || error;
+            return completedCalled || errorCalled;
         }
     }
 
     private void waitUntilAllDone() {
         while (true) {
-            boolean done = subscriber1.done() && subscriber2.done() && subscriber3.done();
+            boolean done = subscriber1.done() && 
+                           subscriber2.done() && 
+                           subscriber3.done();
+            
             if (done) {
                 break;
             } else {
@@ -139,5 +123,25 @@ public class OnErrorRetryCacheTest {
         try {
             Thread.sleep(millis);
         } catch (InterruptedException ignore) {}
+    }
+
+    private Observable<Integer> taskThatFailsOnFirstAttempt() {
+        return longTask
+                .map(x -> {
+                    if (attempts.get() == 1) {
+                        throw new RuntimeException();
+                    }
+                    return 0;
+                });
+    }
+
+    private Observable<Integer> taskThatFailsOnFirstTwoAttempts() {
+        return longTask
+                .map(x -> {
+                    if (attempts.get() <= 2) {
+                        throw new RuntimeException();
+                    }
+                    return 0;
+                });
     }
 }
